67860a9c1d40bfb24fbeb53c0604fefbc188ac13
GabieRise <merrygabby14@gmail.com>
2026-01-31 21:34:42 +0100

    addition of links for resources data

--- src/data/resources.js
+++ src/data/resources.js
@@ -5,7 +5,8 @@ const resourcesData = [
     description: "HTML defines the structure of web pages.",
     links: [
       { name: "MDN HTML", url: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
-      { name: "W3Schools HTML", url: "https://www.w3schools.com/html/" }
+      { name: "W3Schools HTML", url: "https://www.w3schools.com/html/" },
+      {name: "FreeCodeCamp HTML", url: "https://www.freecodecamp.org/learn/"}
     ]
   },
   {
@@ -14,7 +15,8 @@ const resourcesData = [
     description: "CSS controls layout, colors, and responsiveness.",
     links: [
       { name: "MDN CSS", url: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
-      { name: "CSS-Tricks", url: "https://css-tricks.com/" }
+      { name: "CSS-Tricks", url: "https://css-tricks.com/" },
+      {name: "W3Schools CSS Tutorial", url: "https://www.w3schools.com/css/"}
     ]
   },
   {
@@ -23,7 +25,8 @@ const resourcesData = [
     description: "JavaScript adds interactivity and logic.",
     links: [
       { name: "MDN JS", url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
-      { name: "JavaScript.info", url: "https://javascript.info/" }
+      { name: "JavaScript.info", url: "https://javascript.info/" },
+      {name: "W3Schools JavaScript Turorial", url: "https://www.w3schools.com/js/"}
     ]
   }
 ];
